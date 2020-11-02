7585861bc57161a6e51574b81d81ffe47bc3078a
Jayden Chan <jaydencn7@gmail.com>
2020-11-02 02:46:47 -0700

    JC: Basic discrete palette heatmap generation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { generate } from "./svg";
+import { GITHUB_PALETTE_OLD, GITHUB_PALETTE } from "./types";
+
 function main() {
-  console.log("Hello world");
+  const data = [2];
+  for (let i = 0; i < 364; i++) {
+    data.push(Math.floor(Math.random() * 5));
+  }
+  console.log(generate(data, 2019, GITHUB_PALETTE));
 }
 
 main();
---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export type Coord = [number, number];
+
+export const GITHUB_PALETTE_OLD = [
+  "#eeeeee",
+  "#c6e48b",
+  "#7bc96f",
+  "#239a3b",
+  "#196127",
+  "#000000",
+];
+
+export const GITHUB_PALETTE = [
+  "#ebedf0",
+  "#9be9a8",
+  "#40c463",
+  "#30a14e",
+  "#216e39",
+];
